da1a26579a4350216330b52dd5850773c60e284f
Khushi Kumari <muskankumari00999.mk@gmail.com>
2025-08-19 11:10:12 +0530

    Update error Of Project Dependency

--- Template/Template.API/Controllers/TemplateController.cs
+++ Template/Template.API/Controllers/TemplateController.cs
@@ -1,11 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using MediatR;
-using System.Threading.Tasks;
-using Template.Application.DTO.Request;
-using Template.Application.DTO.Response;
-using Template.Application.Restaurant.Commands;
-using Template.Application.Restaurant.Queries;
-
 namespace Template.API.Controllers
 {
     [ApiController]
--- Template/Template.API/Program.cs
+++ Template/Template.API/Program.cs
@@ -1,3 +1,5 @@
+using Template.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services
+    .AddApplicationService(builder.Configuration)
+    .AddAPIService(builder.Configuration)
+    .AddInfrastructureService(builder.Configuration);
+builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
