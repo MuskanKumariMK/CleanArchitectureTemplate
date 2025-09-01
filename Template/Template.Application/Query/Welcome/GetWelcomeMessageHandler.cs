d90d0711e9b766344993a266024321936d0fb0ee
Khushi Kumari <muskankumari00999.mk@gmail.com>
2025-09-01 13:26:37 +0530

    Add Get Welcom Using Pipeline

---
+++ Template/Template.Application/Query/Welcome/GetWelcomeMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Template.Application.Query.Welcome
+{
+     /// <summary>
+     /// Handler for the GetWelcomeMessageQuery
+     /// </summary>
+     public class GetWelcomeMessageHandler : IRequestHandler<GetWelcomeMessageQuery, GetWelcomeMessageResult>
+     {
+          public Task<GetWelcomeMessageResult> Handle(GetWelcomeMessageQuery request, CancellationToken cancellationToken)
+          {
+               var result = new GetWelcomeMessageResult("Welcome to the Clean Architecture Microservices Template!");
+               return Task.FromResult(result);
+          }
+     }
+}
---
+++ Template/Template.Application/Query/Welcome/GetWelcomeMessageQuery.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlock.Behaviour.Interface;
+using BuildingBlock.CQRS;
+
+namespace Template.Application.Query.Welcome
+{
+     /// <summary>
+     /// Query to request the welcome message.
+     /// </summary
+     public record GetWelcomeMessageQuery : IQuery<GetWelcomeMessageResult>;
+
+     /// <summary>
+     /// Result returned for the welcome message query.
+     /// </summary>
+     /// <param name="Message">The welcome message text.</param>
+     public record GetWelcomeMessageResult(string result);
+     public static class Welcome
+     {
+          public class Authorization : IAuthorizationService<GetWelcomeMessageQuery>
+          {
+               public Task Authorize(GetWelcomeMessageQuery request, CancellationToken cancellationToken)
+               {
+                    return Task.CompletedTask;
+               }
+          }
+
+          public class Logger : ILoggerService<GetWelcomeMessageQuery>
+          {
+               public Task Log(GetWelcomeMessageQuery request, string result)
+               {
+                    return Task.CompletedTask;
+               }
+          }
+     }
+}
